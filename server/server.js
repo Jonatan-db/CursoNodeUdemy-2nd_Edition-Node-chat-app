502dbb237007297474e9bb6fdcc29a554cebcb2f
HSNBDES02\jbonet <jbonet@hasar.com>
2017-09-18 09:57:56 -0300

    Iclusión de Socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
-
-const app = express();
-
-app.use(express.static(publicPath));
+const http = require('http');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
+const app = express();
+var server = http.createServer(app)
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('Nuevo usuario conectado');
+    socket.on('disconnect', () => {
+        console.log('El usuario se desconectó.');
+    })
+});
+
+
+server.listen(port, ()=>{
     console.log(`Servidor corriendo en: http://localhost:${port}`);
 });
 
